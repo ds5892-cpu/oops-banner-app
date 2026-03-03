4f865be383878ad6a6ab5ed2336c57c14a98e028
ds5892-cpu <ds5892@srmist.edu.in>
2026-03-03 16:36:01 +0530

    UC6 - Implemented static helper methods for O, P, S

---
+++ BannerAppUC6.java
@@ -0,0 +1,43 @@
+public class BannerAppUC6 {
+
+    public static String[] buildO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    public static String[] buildP() {
+        return new String[]{
+                "*****  ",
+                "*    * ",
+                "*****  ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    public static String[] buildS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                " ***** ",
+                "      *",
+                " ***** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
+
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "   " + p[i] + "   " + s[i]);
+        }
+    }
+}
