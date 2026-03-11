30c4a8d92a8d0e71085cc3a4c8c83001146ed37a
ds5892-cpu <ds5892@srmist.edu.in>
2026-03-11 08:43:02 +0530

    Create OOPSBannerAppUC3.java

---
+++ OOPSBannerAppUC3.java
@@ -0,0 +1,52 @@
+/**
+ * OOPS Banner App
+ * UC3 - Using String.join() to build banner lines
+ */
+
+public class OOPSBannerAppUC3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                " ***** ",
+                " ***** ",
+                " ***** ",
+                " ***** "));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*"));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*"));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                " ***** "));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*",
+                "     *"));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*",
+                "     *"));
+
+        System.out.println(String.join(" ",
+                " ***** ",
+                " ***** ",
+                "*",
+                " ***** "));
+    }
+}
